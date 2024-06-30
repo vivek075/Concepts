71755d6701b0681013e00c4bc9f162fdf555b106
Vivek Singh <30685011+vivek075@users.noreply.github.com>
2024-06-30 10:57:14 +0530

    Update ImmutableExample.java

--- ImmutableExample.java
+++ ImmutableExample.java
@@ -1,6 +1,12 @@
 import java.util.HashMap;
 import java.util.Map;
-
+/*
+The class must be declared as final so that child classes can’t be created.
+Data members in the class must be declared private so that direct access is not allowed.
+Data members in the class must be declared as final so that we can’t change the value of it after object creation.
+A parameterized constructor should initialize all the fields performing a deep copy so that data members can’t be modified with an object reference.
+Deep Copy of objects should be performed in the getter methods to return a copy rather than returning the actual object reference)
+**/
 final class Student {
 	private final String name;
 	private final int regNo;
